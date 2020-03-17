7199810f9f9aea1e62de887620639ae0c15dccca
TalhaAzam <talhasahi86@gmail.com>
2020-03-17 17:30:46 +0500

    Initial Commit

---
+++ ScreenLock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScreenLock
+//
+//  Created by Talha on 17/03/2020.
+//  Copyright © 2020 Talha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
